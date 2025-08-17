df62991fedd7e0f9974b0d2b86a3555c75e8b3a4
GlazovY <glazov.y0@gmail.com>
2025-08-18 01:31:04 +0300

    adding new components by drag

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -40,9 +40,7 @@ if (menuContainerElement && layoutElement) {
     newItem.textContent = c.name;
     menuContainerElement.appendChild(newItem);
 
-    newItem.addEventListener("click", () => {
-      goldenLayout.addComponent(c.name, c.factory);
-    });
+    goldenLayout.newDragSource(newItem, c.name, c.factory);
   });
 
   window.addEventListener("resize", () => {
